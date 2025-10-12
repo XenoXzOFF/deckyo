af87277d61793fdf1f8b13afa8920fc96f5dfbae
XenoXzOFF <xenoxzperso@gmail.com>
2025-10-12 19:06:51 +0200

    Autorole

---
+++ cogs/autorole.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+import os
+import datetime
+
+AUTOROLE_ID = int(os.getenv('AUTOROLE_ID'))
+
+class AutoRole(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        role = member.guild.get_role(AUTOROLE_ID)
+        if role:
+            try:
+                await member.add_roles(role)
+                print(f"✅ Rôle {role.name} ajouté à {member}.")
+            except discord.Forbidden:
+                print(f"⚠️ Impossible d'ajouter le rôle à {member}. Permissions insuffisantes.")
+            except Exception as e:
+                print(f"⚠️ Erreur lors de l'ajout du rôle à {member} : {e}")
+        else:
+            print(f"⚠️ Rôle introuvable : {AUTOROLE_ID}")
+
+async def setup(bot):
+    await bot.add_cog(AutoRole(bot))
