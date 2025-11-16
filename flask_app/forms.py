eac06384b237a71f25be8c378197e6e407f8ff58
XenoXzOFF <xenoxzperso@gmail.com>
2025-11-16 11:25:01 +0100

    Add registration and login forms, update requirements
    Introduced RegistrationForm and LoginForm classes in forms.py to support user registration and authentication. Updated requirements.txt to include Flask-Bcrypt for password hashing.

--- flask_app/forms.py
+++ flask_app/forms.py
@@ -1,7 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Regexp
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, EqualTo, Regexp
 
 class RequestResetForm(FlaskForm):
     discord_id = StringField('Votre ID Discord', validators=[DataRequired(), Regexp(r'^\d+$', message="L'ID Discord ne doit contenir que des chiffres.")])
     submit = SubmitField('Recevoir le code')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Nom d\'utilisateur', validators=[DataRequired(), Length(min=2, max=20)])
+    discord_id = StringField('Votre ID Discord', validators=[DataRequired(), Regexp(r'^\d+$', message="L'ID Discord ne doit contenir que des chiffres.")])
+    password = PasswordField('Mot de passe', validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField('Confirmer le mot de passe', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('S\'inscrire')
+
+class LoginForm(FlaskForm):
+    username = StringField('Nom d\'utilisateur', validators=[DataRequired()])
+    password = PasswordField('Mot de passe', validators=[DataRequired()])
+    remember = BooleanField('Se souvenir de moi')
+    submit = SubmitField('Se connecter')
